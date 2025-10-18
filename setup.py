780afeedcb8ff21d7e6ea6ac8fbbf1bfccefbd96
mark55hun <schvannermark55@gmail.com>
2025-10-18 23:15:55 +0200

    Small fixes to setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ package_name = 'robot_status_monitor'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=['src'],  # find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'}, 
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
